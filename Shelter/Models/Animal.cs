8672683a2718ecb459b0a604c021731b08d3efff
Alex Adamovic <alexanderadamovic@gmail.com>
2022-03-08 15:46:30 -0800

    add Epicodus practice project Animal Shelter
    Co-authored-by: Andy Plymate <Plymatea@users.noreply.github.com>
    Co-authored-by: jwtill <jwtill@users.noreply.github.com>

---
+++ Shelter/Models/Animal.cs
@@ -0,0 +1,11 @@
+namespace Shelter.Models 
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string DateIn { get; set; }
+    public string Breed { get; set; }
+  }
+}
